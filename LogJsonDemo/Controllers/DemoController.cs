846a8731c8f811b05358912b8d0518b38a4488ff
suizhikuo <suizhikuo@126.com>
2014-11-09 12:05:15 +0800

    ..

---
+++ LogJsonDemo/Controllers/DemoController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+
+namespace LogJsonDemo.Controllers
+{
+    public class DemoController : Controller
+    {
+        //
+        // GET: /Demo/
+
+        public ActionResult DebugTrance()
+        {
+            System.Diagnostics.Debug.WriteLine("Debug在output面板输出");
+            System.Diagnostics.Trace.WriteLine("Trace在output面板输出");
+
+            System.Diagnostics.Debug.Assert(false, "断言报错.");
+
+            return View();
+        }
+
+
+        public ActionResult ClassJson()
+        {
+            var myclass = new Models.ClassToJson()
+            {
+                id = 1,
+                Name = "nameas",
+                Address = "adizhidafdf",
+                Phone = "asdfasdf",
+            };
+
+            JavaScriptSerializer jss = new JavaScriptSerializer();
+            var jsonstr = jss.Serialize(myclass);
+            System.Diagnostics.Debug.WriteLine(jsonstr);
+
+            string LogText = string.Format("我的文本日志");
+            StreamWriter logWriter = System.IO.File.AppendText(Server.MapPath("~/") + "app.log");
+            logWriter.WriteLine(LogText);
+            logWriter.Close();
+
+            return View();
+        }
+    }
+}
---
+++ LogJsonDemo/Models/ClassToJson.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LogJsonDemo.Models
+{
+    public class ClassToJson
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+    }
+}
